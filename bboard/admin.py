7618b88f1359521e5f94c87162220798176edf94
Mikhail Kudinov <kudinov.mos@gmail.com>
2020-02-29 12:08:03 +0300

    конец главы 1

--- bboard/admin.py
+++ bboard/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
+from .models import Bd
+
+class BdAmin(admin.ModelAdmin):
+    list_display = ('title', 'content', 'price', 'published')
+    list_display_links = ('title', 'content')
+    search_fields = ('title', 'content')
+
+admin.site.register(Bd, BdAmin)
+
 # Register your models here.
--- bboard/views.py
+++ bboard/views.py
@@ -3,5 +3,5 @@ from django.shortcuts import render
 from .models import Bd
 
 def index(request):
-    bbs = Bd.objects.order_by('-published')
+    bbs = Bd.objects.all()
     return render(request, 'bboard/index.html', {'bbs': bbs})
