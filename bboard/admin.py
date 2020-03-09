78a2645724ffca27f321cf36fd46df0a84f436f2
Mikhail Kudinov <kudinov.mos@gmail.com>
2020-03-09 19:36:33 +0300

    2.3

--- bboard/admin.py
+++ bboard/admin.py
@@ -1,12 +1,14 @@
 from django.contrib import admin
 
 from .models import Bd
+from .models import Rubric
 
 class BdAmin(admin.ModelAdmin):
-    list_display = ('title', 'content', 'price', 'published')
+    list_display = ('title', 'content', 'price', 'published', 'rubric')
     list_display_links = ('title', 'content')
     search_fields = ('title', 'content')
 
 admin.site.register(Bd, BdAmin)
+admin.site.register(Rubric)
 
 # Register your models here.
--- bboard/views.py
+++ bboard/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
 
 from .models import Bd
+from .models import Rubric
+
+def by_rubric(request, rubric_id):
+    bbs = Bd.objects.filter(rubric=rubric_id)
+    rubrics = Rubric.objects.all()
+    current_rubric = Rubric.objects.get(pk=rubric_id)
+    context = {'bbs': bbs, 'rubrics': rubrics, 'current_rubric': current_rubric}
+    return render(request, 'bboard/by_rubric.html', context)
 
 def index(request):
     bbs = Bd.objects.all()
-    return render(request, 'bboard/index.html', {'bbs': bbs})
+    rubrics = Rubric.objects.all()
+    context = {'bbs': bbs, 'rubrics': rubrics}
+    return render(request, 'bboard/index.html', context)
